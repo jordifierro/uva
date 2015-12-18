f89960095d9cf05dfdc8cd0d82c524e2e97cf872
jordifierro <jordifierromulero@gmail.com>
2015-12-18 16:05:13 +0100

    Add 1225.cc

---
+++ 1225.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+
+    string line;
+    getline(cin, line);
+    istringstream ss(line);
+
+    int cases = 0;
+    ss >> cases;
+
+    for(int i = 0; i < cases; ++i) {
+
+       	getline(cin, line);
+        ss.clear();
+		ss.str(line);
+
+		int n;
+		ss >> n;
+
+		vector<unsigned long long> v(10);
+		for(int j = 1; j <= n; ++j) {
+            int curr = j;
+            while(curr) {
+                ++v[curr%10];
+                curr /= 10;
+            }
+		}
+
+		for(int j = 0; j < 10; ++j) {
+		  if(j) cout << " ";
+		  cout << v[j];
+		}
+		cout << endl;
+	}
+}
