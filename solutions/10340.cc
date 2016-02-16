c73f7b2883bc6e9a4627186577cb83871404c961
jordifierro <jordifierromulero@gmail.com>
2016-02-16 16:24:48 +0100

    Fix 10340.cc indentation

--- solutions/10340.cc
+++ solutions/10340.cc
@@ -5,16 +5,16 @@ using namespace std;
 
 int main() {
 
-	string line;
+    string line;
     while (getline(cin, line)) {
         istringstream ss(line);
-
+        
         string s, t;
         ss >> s >> t;
 
         int x = 0;
         for (int y = 0; x < s.size() and y < t.size(); ++y)
-            if(s[x] == t[y]) ++x;
+        if (s[x] == t[y]) ++x;
 
         if (x == s.size()) cout << "Yes" << endl;
         else cout << "No" << endl;
